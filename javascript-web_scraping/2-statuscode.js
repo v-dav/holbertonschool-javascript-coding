4f255f01e571254fea21037634358b6fa621c667
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-09-19 03:18:47 -0700

    Write a script that display the status code of a GET request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('code:', response.statusCode);
+});
