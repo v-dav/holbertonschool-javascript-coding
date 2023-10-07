242d95af27d9a93db47db0244354f939721da5e6
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-07 09:56:03 -0700

    remove comments

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-// Affiche le message de bienvenue
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-// Écoute les entrées utilisateur
 process.stdin.on('readable', () => {
-  // Lit l'entrée utilisateur
   const input = process.stdin.read();
   if (input !== null) process.stdout.write(`Your name is: ${input}`);
 });
 
-// Gère la fin de l'entrée utilisateur
+// The end event is trigerred by the entering input
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
