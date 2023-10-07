289b16a829995a085a09d14207ba288159e6c14c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-07 09:52:25 -0700

    reqwrite compeltely the program

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,14 @@
+// Affiche le message de bienvenue
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-	let input = data.toString()
-	process.stdout.write(`Your name is: ${input}\r`);
-	process.stdout.write('This important software is now closing\n');
-	process.exit();
+
+// Écoute les entrées utilisateur
+process.stdin.on('readable', () => {
+  // Lit l'entrée utilisateur
+  const input = process.stdin.read();
+  if (input !== null) process.stdout.write(`Your name is: ${input}`);
+});
+
+// Gère la fin de l'entrée utilisateur
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
