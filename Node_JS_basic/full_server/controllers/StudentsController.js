c3c57f5fb9576a3d7d0f4bf50074a699996b20a4
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-08 07:41:43 -0700

    try to pass the checker

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,15 +6,15 @@ class StudentsController {
   static getAllStudents(request, response) {
     readDatabase(path)
       .then((data) => {
-        const fields = Object.keys(data).sort();
+				const fields = Object.keys(data).sort();
         const results = ['This is the list of our students'];
         for (const field of fields) {
           const names = data[field];
           const count = names.length;
           const list = names.join(', ');
           results.push(`Number of students in ${field}: ${count}. List: ${list}`);
-        }
-        response.status(200);
+				}
+				response.status(200);
         response.send(results.join('\n'));
       })
       .catch((err) => {
@@ -24,16 +24,22 @@ class StudentsController {
   }
 
   static getAllStudentsByMajor(request, response) {
-    const field = request.params.major;
-    readDatabase(path).then((students) => {
-      if (!(field in students)) {
-        response.status(500).send('Major parameter must be CS or SWE');
-      } else {
-        response.status(200).send(`List: ${students[field].join(', ')}`);
-      }
-    }).catch(() => {
-      response.status(500).send('Cannot load the database');
-    });
+    const requestUrl = request.params.major;
+    const match = requestUrl.match(/CS|SWE/);
+    if (!match) {
+      response.status(500);
+      response.send('Major parameter must be CS or SWE');
+    }
+    readDatabase(path)
+      .then((data) => {
+				const namesList = data[match];
+				response.status(200);
+        response.send(`List: ${namesList.join(', ')}`);
+      })
+      .catch((err) => {
+        response.status(500);
+        response.send(err.message);
+      });
   }
 }
 module.exports = StudentsController;
